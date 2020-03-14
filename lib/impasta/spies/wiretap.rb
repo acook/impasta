2341b0cb6d7be02cfb0be7b919d59aefe81a3b65
Anthony Cook <github@anthonymcook.com>
2020-03-14 18:38:10 -0500

    Updating and fixing Wiretap spy.

--- lib/impasta/spies/wiretap.rb
+++ lib/impasta/spies/wiretap.rb
@@ -3,7 +3,14 @@ require_relative "spy"
 module Impasta
   class Wiretap < Spy
     def __impasta_method name, args, block
-      impasta.object.public_send :name, *args, &block
+      @__impasta_deaddrop = impasta.target.public_send name, *args, &block
+      self
+    end
+
+    def method_missing name, *args, &block
+      super name, *args, &block
+      value, @__impasta_deaddrop = @__impasta_deaddrop, nil
+      value
     end
   end
 end
